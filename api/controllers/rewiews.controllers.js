6e183cb824eb90657daab77caae91005ce30e1a4
fatfroggo <lilybaird1@gmail.com>
2022-11-15 07:54:16 +0000

    updated review comments functionality to allow for no error when given a valid id which does not have any comments

--- api/controllers/rewiews.controllers.js
+++ api/controllers/rewiews.controllers.js
@@ -1,36 +1,40 @@
-const { selectReviews, selectReviewComments, selectReviewsById } = require("../models/reviews.models.js")
+const {
+  selectReviews,
+  selectReviewComments,
+  selectReviewsById,
+} = require("../models/reviews.models.js");
 
 exports.getReviews = (req, res, next) => {
-    selectReviews() 
+  selectReviews()
     .then((reviews) => {
-        res.status(200).send({ reviews })
+      res.status(200).send({ reviews });
     })
     .catch((err) => {
-        next(err)
-    })
-}
+      next(err);
+    });
+};
 
 exports.getReviewComments = (req, res, next) => {
-    const { review_id } = req.params;
-    selectReviewComments(review_id)
+  const { review_id } = req.params;
+  selectReviewsById(review_id)
+    .then(() => {
+      return selectReviewComments(review_id);
+    })
     .then((comments) => {
-        res.status(200).send({ comments })
+      res.status(200).send({ comments });
     })
     .catch((err) => {
-        next(err)
-    })
- }
- 
- exports.getReviewsById = (req, res, next) => {
-    const { review_id } = req.params;
-    selectReviewsById(review_id)
+      next(err);
+    });
+};
+
+exports.getReviewsById = (req, res, next) => {
+  const { review_id } = req.params;
+  selectReviewsById(review_id)
     .then((review) => {
-        res.status(200).send({ review })
+      res.status(200).send({ review });
     })
     .catch((err) => {
-        next(err)
-    })
-}
-
-
-
+      next(err);
+    });
+};
