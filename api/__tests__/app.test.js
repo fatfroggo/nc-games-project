8db91ffe34169a209fccb119fb8eb5d53b944dbb
fatfroggo <lilybaird1@gmail.com>
2022-11-15 15:46:23 +0000

    removed duplicates from merges

--- api/__tests__/app.test.js
+++ api/__tests__/app.test.js
@@ -87,39 +87,39 @@ describe("/api/reviews?category", () => {
   });
   test("Returns an empy array when given a category which has no related reviews", () => {
     return request(app)
-    .get("/api/reviews?category=children's%20games")
-    .expect(200)
-    .then(({ body }) => {
-      expect(body.reviews.length).toBe(0)
-    })
-  })
+      .get("/api/reviews?category=children's%20games")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.reviews.length).toBe(0);
+      });
+  });
   test("Returns a 404 not found error when given an invalid category query", () => {
     return request(app)
-    .get("/api/reviews?category=farmland")
-    .expect(404)
-    .then(({ body }) => {
-      expect(body.msg).toBe("Not found")
-    })
-  })
+      .get("/api/reviews?category=farmland")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Not found");
+      });
+  });
   test("Returns a 404 not found error when given an invalid category query", () => {
     return request(app)
-    .get("/api/reviews?category=7")
-    .expect(404)
-    .then(({ body }) => {
-      expect(body.msg).toBe("Not found")
-    })
-  })
+      .get("/api/reviews?category=7")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Not found");
+      });
+  });
 });
 
 describe("/api/reviews?sort_by", () => {
   test("GET 200 - returns an array of sorted reviews when given a valid sort by query", () => {
     return request(app)
-    .get("/api/reviews?sort_by=designer")
-    .expect(200)
-    .then(({ body }) => {
-      expect(body.reviews).toBeSortedBy("designer", { descending: true })
-    })
-  })
+      .get("/api/reviews?sort_by=designer")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.reviews).toBeSortedBy("designer", { descending: true });
+      });
+  });
   test("GET 400 - invalid sort query", () => {
     return request(app)
       .get("/api/reviews?sort_by=something_bad")
@@ -128,7 +128,7 @@ describe("/api/reviews?sort_by", () => {
         expect(body.msg).toBe("Invalid sort query");
       });
   });
-})
+});
 
 describe("/api/reviews?order", () => {
   test("GET 200 : response object is sorted by date ascending", () => {
@@ -159,16 +159,14 @@ describe("/api/reviews/:review_id/comments", () => {
         expect(comments).toBeInstanceOf(Array);
         comments.forEach((comment) => {
           expect(comment).toEqual(
-            
             expect.objectContaining({
-                comment_id: expect.any(Number),
-                votes: expect.any(Number),
-                created_at: expect.any(String),
-                author: expect.any(String),
-                body: expect.any(String),
-                review_id: expect.any(Number),
-              })
-          
+              comment_id: expect.any(Number),
+              votes: expect.any(Number),
+              created_at: expect.any(String),
+              author: expect.any(String),
+              body: expect.any(String),
+              review_id: expect.any(Number),
+            })
           );
         });
       });
@@ -180,17 +178,17 @@ describe("/api/reviews/:review_id/comments", () => {
       .then(({ body }) => {
         const { comments } = body;
         expect(comments).toBeInstanceOf(Array);
-        expect(comments.length).toBe(0);;
-          });;
-    });;
+        expect(comments.length).toBe(0);
+      });
+  });
   test("GET 400 - returns an error if given an invalid data type for review_id", () => {
     return request(app)
-        .get("/api/reviews/hello/comments")
-        .expect(400)
-        .then(({ body }) => {
-        expect(body.msg).toEqual("Bad request");;
-        });;
-  });;
+      .get("/api/reviews/hello/comments")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toEqual("Bad request");
+      });
+  });
   test("GET 404 - returns an error if given a valid but non existent input for review_id", () => {
     return request(app)
       .get("/api/reviews/999/comments")
@@ -202,44 +200,6 @@ describe("/api/reviews/:review_id/comments", () => {
 });
 
 describe("/api/reviews/review:id", () => {
-    test("GET 200 - returns a review object corresponding to the given review id", () => {
-      return request(app)
-        .get("/api/reviews/9")
-        .expect(200)
-        .then(({ body }) => {
-          const { review } = body;
-          expect(review).toEqual(
-            expect.objectContaining({
-              review_id: 9,
-              title: 'A truly Quacking Game; Quacks of Quedlinburg',
-              review_body: expect.any(String),
-              designer: expect.any(String),
-              review_img_url: expect.any(String),
-              votes: 10,
-              category: 'social deduction',
-              owner: 'mallionaire',
-              created_at: expect.any(String),
-              comment_count: expect.any(Number)
-            })
-          );
-        });
-    });
-    test("GET 404 - returns a 404 not found error when given a nonexistent review_id", () => {
-      return request(app)
-        .get("/api/reviews/90")
-        .expect(404)
-        .then(({ body }) => {
-          expect(body.msg).toEqual("Not found")
-        })
-    })
-    test("GET 400 - returns a bad request error when given an invalid id", () => {
-        return request(app)
-        .get("/api/reviews/hello")
-        .expect(400)
-        .then(({ body }) => {
-          expect(body.msg).toEqual("Bad request")
-        })
-    })
   test("GET 200 - returns a review object corresponding to the given review id", () => {
     return request(app)
       .get("/api/reviews/9")
@@ -250,15 +210,14 @@ describe("/api/reviews/review:id", () => {
           expect.objectContaining({
             review_id: 9,
             title: "A truly Quacking Game; Quacks of Quedlinburg",
-            review_body:
-              "Ever wish you could try your hand at mixing potions? Quacks of Quedlinburg will have you mixing up a homebrew like no other. Each player buys different ingredients (chips) that are drawn at random to reach the most points, but watch out, you'd better not let your cauldrom explode.",
-            designer: "Wolfgang Warsch",
-            review_img_url:
-              "https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg",
+            review_body: expect.any(String),
+            designer: expect.any(String),
+            review_img_url: expect.any(String),
             votes: 10,
             category: "social deduction",
             owner: "mallionaire",
             created_at: expect.any(String),
+            comment_count: expect.any(Number),
           })
         );
       });
@@ -370,30 +329,29 @@ describe("Update a given review", () => {
   });
 });
 
-  describe("/api/users", () => {
-    test("GET 200 - returns an array of user objects in the correct format", () => {
-      return request(app)
+describe("/api/users", () => {
+  test("GET 200 - returns an array of user objects in the correct format", () => {
+    return request(app)
       .get("/api/users")
       .expect(200)
       .then(({ body }) => {
         const { users } = body;
-        expect(users).toBeInstanceOf(Array)
-        expect(users.length).toBe(4)
+        expect(users).toBeInstanceOf(Array);
+        expect(users.length).toBe(4);
         users.forEach((user) => {
           expect.objectContaining({
             username: expect.any(String),
             name: expect.any(String),
-            avatar_url: expect.any(String)
-          })
-        })
-      })
-    })
-  })
+            avatar_url: expect.any(String),
+          });
+        });
+      });
+  });
+});
 
-  
 describe("Adds a comment to a given review", () => {
-    test("POST - 201, adds a comment to a given review and returns that comment", () => {
-      return request(app)
+  test("POST - 201, adds a comment to a given review and returns that comment", () => {
+    return request(app)
       .post("/api/reviews/5/comments")
       .send({
         username: "dav3rid",
@@ -433,27 +391,24 @@ describe("Adds a comment to a given review", () => {
   });
 });
 
-
 describe("Delete a comment when given a valid comment id", () => {
   test("Deletes a comment when given a comment id", () => {
-    return request(app)
-    .delete("/api/comments/2")
-    .expect(204)
-  })
+    return request(app).delete("/api/comments/2").expect(204);
+  });
   test("Returns a 404 not found if provided a valid but non existent comment id", () => {
     return request(app)
-    .delete("/api/comments/50")
-    .expect(404)
-    .then(({ body }) => {
-      expect(body.msg).toBe("Not found")
-    })
-  })
+      .delete("/api/comments/50")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Not found");
+      });
+  });
   test("Returns a 400 bad request if provided an invalid comment id", () => {
     return request(app)
-    .delete("/api/comments/hello")
-    .expect(400)
-    .then(({ body }) => {
-      expect(body.msg).toBe("Bad request")
-    })
-  })
-})
+      .delete("/api/comments/hello")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Bad request");
+      });
+  });
+});
