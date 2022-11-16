cd640d58aec0f8a9cf79170bf63c74789a608153
fatfroggo <lilybaird1@gmail.com>
2022-11-16 11:44:46 +0000

    merge conflicts and refatored

--- api/app.js
+++ api/app.js
@@ -6,9 +6,6 @@ app.use(express.json());
 
 app.use("/api", apiRouter)
 
-app.get("/api/users/:username", getUserByUsername)
-
-
 app.use((err, req, res, next) => {
   if (err.status) {
     res.status(err.status).send({ msg: err.msg });
