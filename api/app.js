e058493a93a5aed951762ec583152c5028784bcf
fatfroggo <lilybaird1@gmail.com>
2022-12-05 11:13:25 +0000

    update

--- api/app.js
+++ api/app.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const apiRouter = require("../api/routers/api.routers.js")
 
-const cors = require("cors");
-
-app.use(cors());
-
 const app = express();
 app.use(express.json());
 
